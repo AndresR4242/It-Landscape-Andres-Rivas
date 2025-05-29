29b5176b8578bb0ef03270f9a1edf4b979d29d05
AndresR4242 <Andresrivas.onenglish@gmail.com>
2025-05-29 18:08:48 +0200

    Testing Git action

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,4 +1,4 @@
-const GREETING = 'HeLlo My name is Andres , 4 First CI-CD pipeline :) :)!';
+const GREETING = 'HeLlo My name is Andres , 5 First CI-CD pipeline :) :)!';
 
 module.exports = async (req, res) => {
     res.send({
